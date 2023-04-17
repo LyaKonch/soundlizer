b21b641d8618637f25860f25782f30365cf25fb8
LeJuilenne <analnijmonstr@gmail.com>
2023-04-17 20:14:33 +0300

    тяжело... тяжело

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{lazy, useEffect, useState} from 'react';
+import {sounds,soundsBlack} from './soundeffects/sounds.js';
+import { Slider } from '@mui/material';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePause } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+   const [soundsArray, setNewSounds] = useState(sounds);
+   const [value, setValue] = useState(30);
+   const keyArray = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+   const [audio, setAudio] = useState(new Audio());
+   const [display, setDisplay] = useState("");
+   const [styles,setStyles]=useState({
+      float:"left"
+   })
+   const [showIframe,setShowIframe]=useState(false);
+   const [active,setActive]=useState(false);
+
+   function handleCLick(id){
+      let [soundPath] = soundsArray.filter(elem => elem.name==id);
+      audio.src = soundPath.path;
+      audio.volume = value / 100;
+      audio.load();
+      audio.play();
+      setDisplay(soundPath.name);
+   }
+   useEffect(() => {
+      document.addEventListener("keydown", handleKeyEvents);
+      document.addEventListener("keyup", handleKeyEventsStyles);
+      return () => {
+         document.removeEventListener("keydown", handleKeyEvents);
+         document.addEventListener("keyup", handleKeyEventsStyles);
+      };
+   }, [soundsArray]);
+
+   function handleChange(){
+      if(sounds === soundsArray){
+         setNewSounds(soundsBlack);
+         setDisplay("Національний банк чорного гумору");
+         setStyles({
+            float:"right"
+         })
+      } else {
+         setNewSounds(sounds);
+         setDisplay("Національний банк білого гумору");
+         setStyles({
+            float:"left"
+         })
+      }
+   }
+   function handleKeyEventsStyles(e){
+      for (let a of document.querySelectorAll("button")) {
+         let b=[...a.classList];
+         if(b.includes('active')){
+            a.classList.toggle('active');
+         }
+      }
+   }
+
+   function handleKeyEvents(e){
+      let [soundObject] = arrayForRender.filter(elem => elem.props.keyIndex == e.code.slice(3,));
+      if(soundObject != undefined){
+         let [soundPath] = soundsArray.filter(elem => elem.name == soundObject.key);
+         for (let a of document.querySelectorAll("button")) {
+            if (a.textContent.includes(e.code.slice(3,))) {
+              a.classList.toggle('active');
+            }
+         }
+         audio.src = soundPath.path;
+         audio.load();
+         audio.play();
+         setDisplay(soundObject.key);
+      }
+      if(e.code.slice(3,)=="Z"){
+         setShowIframe(true);
+      }else{
+         setShowIframe(false);
+      }
+      if(e.code==="Space"){
+         audio.currentTime=0;
+         audio.pause();
+         return;
+      }
+   }
+
+   function audioFlow(){
+      audio.currentTime=0;
+      audio.pause();
+   }
+
+
+   const handleChanges = (event, newValue) => {
+      setValue(newValue);
+      audio.volume = newValue / 100;
+      setDisplay("Volume: " + Math.floor(audio.volume*100));
+   };
+
+   const arrayForRender = soundsArray.map((elem, index) => (
+      <Button key={elem.name} clickHandler={handleCLick} id={elem.name} keyIndex={keyArray[index] } />
+   ));
+   return (
+      <>
+      {showIframe && <iframe className="video" src="https://www.youtube.com/embed/mdK5Maa0RTI?autoplay=1&controls=0&frameborder=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen muted="true"></iframe>}
+      <div className="wrapper">
+
+         <div className="buttons-area" >
+            {arrayForRender}
+         </div>
+         <div className='interactive-area'>
+            <p className='display-tab'>{display}</p>
+            <Box sx={{ width: "100%"}}>
+               <Stack spacing={2} direction="row" sx={{ mb: 0 }} alignItems="center">
+                  <Slider aria-label="Volume" value={value} onChange={handleChanges} />
+               </Stack>
+            </Box>
+            <div className='stop-control'>
+               <p>Стоп</p>
+               <div onClick={audioFlow} className='stop'>
+                  <FontAwesomeIcon icon={faCirclePause} className='stop-icon'/>
+               </div>
+            </div>
+            <div className='control'>
+               <p>Зміни набір аудіо</p>
+               <div onClick={handleChange} className='select'>
+                  <div className='select-inner' style={styles}></div>
+               </div>
+            </div>
+         </div>
+
+
+      </div>
+      </>
+   );
+}
+
+function Button(props){
+   return (
+      <button className={`button`} onClick={(e)=>props.clickHandler(props.id)} >{props.keyIndex}</button>
+   );
+}
+export default App;
---
+++ src/soundeffects/sounds.js
@@ -0,0 +1,73 @@
+import error from './error1.mp3'
+import moaning from './обережно.mp3'
+import cool from './япокакав.mp3'
+import salo from './сало.mp3'
+import ohshit from './нічогособі.mp3'
+import nineor10 from './девятьчидесять.mp3'
+import girl from './дівчинауенсдей.mp3'
+import nigger from './негрі_чорнийгумор.mp3'
+import wideputin from './wideputin.mp3'
+import oksana from './відос.mp4'
+import artur from './артур.mp4'
+import obeme from './obeme.mp4'
+import trivoga from './trivoga.mp4'
+
+export let sounds=[{
+      name:"BrainLag",
+      path:error
+   },{
+      name:"Обережно",
+      path:moaning
+   },{
+      name:"Я покакав",
+      path:cool
+   },{
+      name:"Сало",
+      path:salo
+   },{
+      name:"Нічого собі",
+      path:ohshit
+   },{
+      name: "Зрада?",
+      path:"https://us-tuna-sounds-files.voicemod.net/ff532925-4153-430c-bb61-e359d033ecfa-1680956362071.mp3"
+   },{
+      name:"Карась крутиться",
+      path:"https://us-tuna-sounds-files.voicemod.net/0bd2c63c-b8e2-4528-9398-4a53c490d1c3-1680955589174.mp3"
+   },{
+      name:"LMAO Мемного права не існує",
+      path:"https://us-tuna-sounds-files.voicemod.net/5e28417a-3b8b-478a-ab0e-a919a20159a4-1664493621383.mp3"
+   },{
+      name:"Thank you much for ♂cumming♂",
+      path:"https://us-tuna-sounds-files.voicemod.net/231404cb-0e6e-4f8c-ad04-7f7c6f0dfbf3-1651899321612.mp3"
+   }
+];
+export let soundsBlack=[
+   {
+      name:"Bugatti",
+      path:"https://us-tuna-sounds-files.voicemod.net/6c9bfca2-4183-4326-8078-21f21b8f264e-1680871856209.mp3"
+   },{
+      name:"Дівчина Середа",
+      path:girl
+   },{
+      name:"База",
+      path:nigger
+   },{
+      name:"Вже не широкий",
+      path:wideputin
+   },{
+      name:"Все ж таки десята ранку",
+      path:nineor10
+   },{
+      name:"ОКСАНА!!",
+      path:oksana
+   },{
+      name:"АРТУР",
+      path:artur
+   },{
+      name:"Лютий мікс",
+      path:obeme
+   },{
+      name:"Досить на сьогодні інтернету",
+      path:trivoga
+   }
+];
