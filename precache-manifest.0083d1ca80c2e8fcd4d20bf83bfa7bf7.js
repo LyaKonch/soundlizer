ac18dc823694f2bc5591991322ff75c9a3cafc44
LeJuilenne <analnijmonstr@gmail.com>
2023-04-17 20:51:40 +0300

    Updates

---
+++ precache-manifest.0083d1ca80c2e8fcd4d20bf83bfa7bf7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "96229d79ff66b5540f1b",
+    "url": "/soundlizer/static/css/main.009302f3.chunk.css"
+  },
+  {
+    "revision": "96229d79ff66b5540f1b",
+    "url": "/soundlizer/static/js/main.96229d79.chunk.js"
+  },
+  {
+    "revision": "0a58e1c4ae58131f766e",
+    "url": "/soundlizer/static/js/1.0a58e1c4.chunk.js"
+  },
+  {
+    "revision": "52527dd5ec2285c8d249",
+    "url": "/soundlizer/static/js/2.52527dd5.chunk.js"
+  },
+  {
+    "revision": "62a9a9a7bc430d5b44f3",
+    "url": "/soundlizer/static/js/runtime~main.62a9a9a7.js"
+  },
+  {
+    "revision": "f3da16101876634bc5072dfbd338b463",
+    "url": "/soundlizer/static/media/error1.f3da1610.mp3"
+  },
+  {
+    "revision": "7f6f91fadedbd7770ae6e2d1b09d6ce2",
+    "url": "/soundlizer/static/media/обережно.7f6f91fa.mp3"
+  },
+  {
+    "revision": "52be52cd5282313ee8461161a1cefbed",
+    "url": "/soundlizer/static/media/япокакав.52be52cd.mp3"
+  },
+  {
+    "revision": "5b71b867f2d72eb31279171f910c8b6a",
+    "url": "/soundlizer/static/media/сало.5b71b867.mp3"
+  },
+  {
+    "revision": "ebe20cbfcec631b01b4fb060995e4db7",
+    "url": "/soundlizer/static/media/нічогособі.ebe20cbf.mp3"
+  },
+  {
+    "revision": "f9c50d66bf8b77616f76f959a3a9bcb7",
+    "url": "/soundlizer/static/media/девятьчидесять.f9c50d66.mp3"
+  },
+  {
+    "revision": "a5d47166e4e5bc141ce430d264c0de04",
+    "url": "/soundlizer/static/media/дівчинауенсдей.a5d47166.mp3"
+  },
+  {
+    "revision": "862d4ed72f137dd54e1c976c0a2c74e7",
+    "url": "/soundlizer/static/media/негрі_чорнийгумор.862d4ed7.mp3"
+  },
+  {
+    "revision": "ce6525e2eb0de26059ffcc45e9be8cad",
+    "url": "/soundlizer/static/media/wideputin.ce6525e2.mp3"
+  },
+  {
+    "revision": "321da80fe2c73bbe904dd5e3f26b689b",
+    "url": "/soundlizer/static/media/відос.321da80f.mp4"
+  },
+  {
+    "revision": "7d6e011096f32884c5d5e85c5068ba8b",
+    "url": "/soundlizer/static/media/артур.7d6e0110.mp4"
+  },
+  {
+    "revision": "e8b2cbb4b12283c2aeaf7ca589fb8814",
+    "url": "/soundlizer/static/media/obeme.e8b2cbb4.mp4"
+  },
+  {
+    "revision": "24c74af902c946b434b8cdfc0c61afde",
+    "url": "/soundlizer/static/media/trivoga.24c74af9.mp4"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/soundlizer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "58c3df23da9836f40d55dcdce48a8c7f",
+    "url": "/soundlizer/index.html"
+  }
+];
